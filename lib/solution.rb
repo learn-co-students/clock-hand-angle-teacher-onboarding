a1f7ec78b4da350abfe34469dee5ed7ecb37af0e
Marc Majcher <majcher@gmail.com>
2020-01-23 10:40:18 -0600

    Done.

--- lib/solution.rb
+++ lib/solution.rb
@@ -1,3 +1,7 @@
 def clock_angle(time)
-    # code your solution here
+  hour, minute = time.split(":").map { |n| n.to_f }
+  hour = (hour == 12) ? 0 : hour
+
+  degrees = ((minute * 6) - (hour * 30 + minute / 2)).abs
+  degrees == 180 ? 180 : degrees % 180
 end
